7d6bac58dd1a15a9ada1678b9139d7f4df56059c
Székely Márton <szekelymarton001@gmail.com>
2022-04-04 11:29:01 +0200

    lajos

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <?php
+$szam = 12;
+$nev = "kecske";
 
-echo file_get_contents('templates/head.tpl');
-echo file_get_contents('templates/nav.tpl');
-echo file_get_contents('templates/home.tpl');
-echo file_get_contents('templates/foot.tpl');
-//asd
+echo "<!DOCTYPE html\n>";
+echo "<html>";
+echo "<body>";
+echo "alag\nsor<br>";
+echo "alag<br>sor";
+echo "<br> Típus: " .gettype($szam);
+
+echo <<<KISKUTYA
+<ul>
+    <li>alma</li>
+    <li>körte</li>
+</ul>
+KISKUTYA;
+
+echo "<br>PHP_OS: " . PHP_OS;
+const MIN_KOR=18;
+echo "<br>Jöhet: " . MIN_KOR;
+
+printf("<br>|%20d|", 58);
+
+echo "<br>";
+echo sqrt(9);
+
+echo "<br";
+$szog = 1;
+echo sin($szog *pi() / 180);
+
+echo "<br>";
+echo pow(2, 8);
+
+echo "</body>";
+echo "</html>";
 
 
